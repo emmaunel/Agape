6bdf7448475800196e57b0f0975f758318b6ec47
Damola Olugboji <damolaolugboji@gmail.com>
2019-08-18 08:38:56 -0400

    almost done

--- ahow-damola.py
+++ ahow-damola.py
@@ -8,8 +8,10 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import Select
 # for now - new series/speakers need to be inputted manually
+# find way to download to specific folder
 
 os.chdir(r'C:\Users\Media\Desktop\sermon auto')
+now = datetime.datetime.now()
 for f in os.listdir():
     str1 = str(now.year)
     str2 = str(now.month)
@@ -209,16 +211,22 @@ def studio():
 
 
 def audiojoiner():
+    now = datetime.datetime.now()
+    str1 = str(now.year)
+    str2 = str(now.month)
+    str3 = str(now.day)
+    date = (" \\" + str2 + "-"+ str3 +"-"+ str1 + ".mp3")    
     driver = webdriver.Chrome(
         executable_path=r"C:\Users\Media\Desktop\webdriver\chromedriver.exe"
     )
     driver.get("https://audio-joiner.com/")
     pathtointro = (r"H:\New Chruch\Podcast Intro Revised MP3.mp3")  # path to podcast intro
-    pathtosermon = (r"C:\Users\Media\Desktop\sermon auto\" + date)
+    pathtosermon = (r"C:\Users\Media\Desktop\sermon auto{}".format(date)) #path to sermon
     print (pathtosermon)
     driver.find_element_by_class_name("file-input").send_keys(pathtointro)
+    time.sleep(5)
+    driver.find_element_by_class_name("file-input").send_keys(pathtosermon)
     driver.find_element_by_class_name("btn-save").click()
-    # need to add path to message
 
     # section below allows for the ability to parse links in the html file and find the download link
     done = input("when it is finished type a letter and hit enter ")
